782d766bf24c7ad14392bd0dee97ce4381390a2c
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-09-18 13:27:37 +0100

    added simple navigation from home screen to register screen

--- app/src/main/java/com/KGRJJ/kgrjj_android_20192020/Authentication/RegistrationActivity.java
+++ app/src/main/java/com/KGRJJ/kgrjj_android_20192020/Authentication/RegistrationActivity.java
@@ -2,15 +2,36 @@ package com.KGRJJ.kgrjj_android_20192020.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.KGRJJ.kgrjj_android_20192020.MainActivity;
 import com.KGRJJ.kgrjj_android_20192020.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+
+    private Button mHomeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mHomeButton = findViewById(R.id.homeBtn_registerScreen);
+
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                launchHomeScreen();
+
+            }
+        });
+    }
+
+    private void launchHomeScreen(){
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/KGRJJ/kgrjj_android_20192020/MainActivity.java
+++ app/src/main/java/com/KGRJJ/kgrjj_android_20192020/MainActivity.java
@@ -2,13 +2,35 @@ package com.KGRJJ.kgrjj_android_20192020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.KGRJJ.kgrjj_android_20192020.Authentication.RegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mRegisterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRegisterButton = findViewById(R.id.signUpBtn_homeScreen);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                LaunchRegisteration();
+            }
+        });
+    }
+
+
+    private void LaunchRegisteration(){
+        Intent myIntent = new Intent(this, RegistrationActivity.class);
+        startActivity(myIntent);
     }
 }
