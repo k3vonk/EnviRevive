45792a19dd54afab937501d916ad44b67a58d63e
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-09-18 11:28:09 +0100

    Adding register activity and new branch

---
+++ app/src/main/java/com/KGRJJ/kgrjj_android_20192020/Authentication/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.KGRJJ.kgrjj_android_20192020.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.KGRJJ.kgrjj_android_20192020.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
