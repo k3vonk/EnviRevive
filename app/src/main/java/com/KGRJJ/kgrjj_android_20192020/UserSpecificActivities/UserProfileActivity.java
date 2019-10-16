6b8ff54665a6b3fb42c57654c2cefd6f5e6f3cc0
Kiowa Daly <kiowa.daly@ucdconnect.ie>
2019-10-16 13:12:07 +0100

    added some basic code for retrieving a photo

--- app/src/main/java/com/KGRJJ/kgrjj_android_20192020/UserSpecificActivities/UserProfileActivity.java
+++ app/src/main/java/com/KGRJJ/kgrjj_android_20192020/UserSpecificActivities/UserProfileActivity.java
@@ -1,14 +1,23 @@
 package com.KGRJJ.kgrjj_android_20192020.UserSpecificActivities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.FileProvider;
+
 import com.KGRJJ.kgrjj_android_20192020.BaseActivity;
+
 import com.KGRJJ.kgrjj_android_20192020.MainActivity;
 import com.KGRJJ.kgrjj_android_20192020.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +25,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+
 
 public class UserProfileActivity extends BaseActivity implements View.OnClickListener{
 
@@ -27,9 +39,11 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
 
     //USER PROFILE PAGE VARIABLES
     private TextView profile_name, profile_rank, profile_city_country, profile_points;
-    private Image profileImage;
+    private ImageView mProfileImage;
     private ImageView image;
+
     private FirebaseUser user;
+    private Uri mProfileImageURI = null;
     //END USER PROFILE VARIABLES
 
     @Override
@@ -45,7 +59,7 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
         profile_rank = findViewById(R.id.profile_rank);
         profile_city_country = findViewById(R.id.profile_city_country);
         profile_points = findViewById(R.id.profile_points);
-
+        mProfileImage = findViewById(R.id.profile_portrait_image);
         user = mAuth.getCurrentUser();
         getFullName(user);
         getRank(user);
@@ -53,7 +67,8 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
         getPoints(user);
 
         findViewById(R.id.profile_sign_out).setOnClickListener(this);
-
+        findViewById(R.id.changeImage).setOnClickListener(this);
+        mProfileImage.setOnClickListener(this);
 
 
 
@@ -108,6 +123,29 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
                     }
                 });
     }
+    protected static final int CAPTURE_IMAGE_ATIVITY_REQUEST_CODE = 0;
+    private void takePhoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        mProfileImageURI = FileProvider.getUriForFile(this,
+                getApplicationContext().getPackageName(),
+                new File(Environment.getExternalStorageDirectory(),
+                "fname_"+ System.currentTimeMillis() +".jpg"));
+
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,mProfileImageURI);
+        startActivityForResult(intent,CAPTURE_IMAGE_ATIVITY_REQUEST_CODE);
+    }
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        if(resultCode==RESULT_OK){
+            if(requestCode == CAPTURE_IMAGE_ATIVITY_REQUEST_CODE){
+                Bundle extras = data.getExtras();
+                Log.e("URI",mProfileImageURI.toString());
+                Bitmap bmp = (Bitmap) extras.get("data");
+                mProfileImage.setImageBitmap(bmp);
+            }
+        }
+    }
 
     @Override
     public void onClick(View v) {
@@ -117,6 +155,9 @@ public class UserProfileActivity extends BaseActivity implements View.OnClickLis
             Intent myIntent = new Intent(this, MainActivity.class);
             Toast.makeText(this,"Starting main activity",Toast.LENGTH_SHORT);
             startActivity(myIntent);
+        }
+        if(i == R.id.changeImage){
+            takePhoto();
         }
     }
 
